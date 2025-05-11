10a4d88a4e6d064a97d7147a49bf5dcb31df58fd
Chhhh <158474029+Chhhh-h@users.noreply.github.com>
2025-05-11 18:15:27 +0800

    Add files via upload

---
+++ .config/helpers/default_font.lua
@@ -0,0 +1,16 @@
+return {
+  text = "JetBrainsMono Nerd Font", -- Used for text
+  numbers = "JetBrainsMono Nerd Font", -- Used for numbers
+
+  -- Unified font style map
+  style_map = {
+    ["Thin"] = "Thin",
+    ["ExtraLight"] = "ExtraLight",
+    ["Light"] = "Light",
+    ["Regular"] = "Regular",
+    ["Medium"] = "Medium",
+    ["Semibold"] = "Semibold",
+    ["Bold"] = "Bold",
+    ["ExtraBold"] = "ExtraBold",
+  }
+}
